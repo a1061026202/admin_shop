f72c6ee485dcc5c06bec9c4b25aa694ce706d381
yecid <yecid@gmail.com>
2022-06-23 15:32:31 +0800

    first commit

---
+++ vue_shop/src/components/Welcome.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        welcome
+    </div>
+</template>
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ vue_shop/src/plugins/element.js
@@ -0,0 +1,64 @@
+/* eslint-disable import/no-duplicates */
+import Vue from 'vue'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Main,
+  Container,
+  Aside,
+  Header,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Main)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Header)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(Header)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
+
+// 全局挂载
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
